c25bbe94ca5ab79b68ab787f935eed360e2348bc
Rahul Jain <rahul.jain1@tothenew.com>
2020-01-30 10:05:14 +0530

    swagger ui integrated

--- books/index.js
+++ books/index.js
@@ -4,9 +4,15 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const route = require('./api/route');
 const dbConnect = require('./db/connection');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 // db connection
 dbConnect()            
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
